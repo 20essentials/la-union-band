09cacc877841d5415269c9ffedab014a6b05e6a0
DominicDev <s0l0estudi4r@gmail.com>
2025-11-22 16:36:40 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'LA UNIÓN ';
+export const nameSongs = [
+	"LOBO HOMBRE EN PARÍS", /*1) (8.3/10)*/
+	"LUNA LLENA EN PARÍS", /*2*/
+	"MARACAIBO ", /*3*/
+	"DONDE ESTABAIS", /*4*/
+	"FUERON LOS CELOS", /*5*/
+	"MÁS Y MÁS", /*6*/
+	"VUELVE EL AMOR", /*7*/
+	"SILDAVIA", /*8*/
+	"VIVIR AL ESTE DEL EDÉN", /*9*/
+	"NEGRITA", /*10*/
+	"ELLA ES UN VOLCAN", /*11*/
+	"LA NIEBLA", /*12*/
+	"MIL SILUETAS", /*13*/
+	"LA TORMENTA", /*14*/
+	"LA MALA VIDA", /*15*/
+	"CARNAVAL", /*16*/
+	"EN TUS LLAMAS", /*17*/
+	"PECADO", /*18*/
+	"SI TE VAS", /*19*/
+	"ALMA DE LADRÓN", /*20*/
+]
